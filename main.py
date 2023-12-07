0148f2ee620e77e268a2889b0bec732b2ad829f2
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-07 14:34:53 +0100

    add 2 def rendom and name

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 import random
@@ -8,4 +10,15 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'example': 'This is an example' , 'data': 0}
+    return {'example': 'This is an example' , 'data': 999}
+
+
+@app.get('/random')
+async def get_random():
+    rn: int = random.randint(0,100)
+    return {'number':rn, 'limt':100}
+
+
+@app.get('/name')
+def name():
+    return {'name': 'Anas'}

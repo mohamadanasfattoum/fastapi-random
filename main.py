2a4295b0457c25cc9f61dd62df4eb7c89fecccee
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-07 13:59:55 +0100

    add show.py to run requeste

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'example': 'This is an example', 'data': 0}
+    return {'example': 'This is an example' , 'data': 0}
---
+++ show.py
@@ -0,0 +1,6 @@
+import requests
+
+
+
+request = requests.get('http://127.0.0.1:8000')
+print(request.json())

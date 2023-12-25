ec7f1d6d9b33b5ace8671103622b4034980aeeca
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-25 15:48:58 +0100

    update the project and fixed

--- show.py
+++ show.py
@@ -2,5 +2,5 @@ import requests
 
 
 
-request = requests.get('http://127.0.0.1:8000')
+request = requests.get('http://127.0.0.1:8000/random/8456421')
 print(request.json())
---
+++ to_run.md
@@ -0,0 +1,11 @@
+pip install fastapi
+
+pip install "uvicorn[standard]"
+
+uvicorn main:app --reload
+
+
+to run show.py we need requests
+
+pip install requests
+
